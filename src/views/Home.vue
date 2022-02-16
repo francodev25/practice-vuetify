4c7ca9d227c997f511077eb076158f61a9d97b8c
Franco Narvaez <juanfranconarvaez@gmail.com>
2022-02-16 16:31:44 -0300

    Update Readme and First steps vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1>Home page</h1>
+  <v-container>
+    <h1 class="red darken-2 green--text text--lighten-4">Home page</h1>
+    <p>YEs</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h1 class="display-1">Massive Display</h1>
+
+    <p class="headline">This is a headline</p>
+    <p class="subheading">This subheading </p>
+    <p class="caption">This subheading </p>
+
+
+  </v-container>
 </template>
 
 <script>
