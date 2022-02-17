d71e10a5fc9fc981f6ed02de6d84aabae6d2609e
Franco Narvaez <juanfranconarvaez@gmail.com>
2022-02-17 15:56:16 -0300

    Basic Navbar and Sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="indigo"
-      dark
-    >
-      <div class="d-flex align-center">
-        Todo App
-      </div>
-
-      <v-spacer></v-spacer>
-    </v-app-bar>
-
+    <navbar/>
+    
     <v-main>
       <router-view/>
     </v-main>
+    
   </v-app>
 </template>
 
 <script>
+import { Navbar } from "./components";
 
 export default {
   name: 'App',
-
+  components:{
+    Navbar
+  },
   data: () => ({
     //
   }),
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <h1 class="red darken-2 green--text text--lighten-4">Home page</h1>
+    <!-- <h1 class="red darken-2 green--text text--lighten-4">Home page</h1>
     <p>YEs</p>
     <h1 class="display-4">Massive Display</h1>
     <h1 class="display-1">Massive Display</h1>
@@ -9,12 +9,28 @@
     <p class="subheading">This subheading </p>
     <p class="caption">This subheading </p>
 
-
+    <v-btn
+      :class="className"
+      class="ma-2 white--text"
+    >
+      Upload
+      <v-icon
+        right
+        dark
+      >
+        mdi-chevron-right
+      </v-icon>
+    </v-btn> -->
   </v-container>
 </template>
 
 <script>
 export default {
   name: "Home",
+  data(){
+    return {
+      className: 'pink hidden-sm-only'
+    }
+  }
 };
 </script>
