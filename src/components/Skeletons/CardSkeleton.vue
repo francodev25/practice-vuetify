b8d06cd4785bf45b45b71d9d6857f38f6998b36c
Franco Narvaez <juanfranconarvaez@gmail.com>
2022-07-19 20:36:34 -0300

    Skeletons Initial

---
+++ src/components/Skeletons/CardSkeleton.vue
@@ -0,0 +1,17 @@
+<template>
+      <v-skeleton-loader
+      class="mx-auto"
+      max-width="300"
+      type="card"
+    ></v-skeleton-loader>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
