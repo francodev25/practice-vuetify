bb20949094585e8dbc9edd4e2eb929020290e2a0
Franco Narvaez <juanfranconarvaez@gmail.com>
2022-07-26 19:01:40 -0300

    Test added

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{msg}}</div>
+</template>
+
+<script>
+export default {
+    name:'HelloWorld',
+    props:{
+        msg:{
+            required:true,
+        }
+    },
+    data(){
+        return{}
+    }
+}
+</script>
+
+<style>
+
+</style>
