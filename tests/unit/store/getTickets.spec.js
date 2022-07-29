3ae58284a4eaee5c4543a96efb67051637bf282e
Franco Narvaez <juanfranconarvaez@gmail.com>
2022-07-29 11:03:29 -0300

    Testing Structure

---
+++ tests/unit/store/getTickets.spec.js
@@ -0,0 +1,23 @@
+import { testAction } from '../helpers'
+const actionsInjector = require('inject-loader!./actions')
+
+// create the module with our mocks
+const actions = actionsInjector({
+  '../api/shop': {
+    getProducts (cb) {
+      setTimeout(() => {
+        cb([ /* mocked response */ ])
+      }, 100)
+    }
+  }
+})
+
+
+describe('actions', () => {
+  it('getAllProducts', done => {
+    testAction(actions.getAllProducts, null, {}, [
+      { type: 'REQUEST_PRODUCTS' },
+      { type: 'RECEIVE_PRODUCTS', payload: { /* mocked response */ } }
+    ], done)
+  })
+})
