1b1f35957001792d6993318d48c9a610703d3efa
Franco Narvaez <juanfranconarvaez@gmail.com>
2022-07-29 11:00:54 -0300

    Autocomplete Cripto

--- src/components/ExpansionPanels/FiltersSearchTicket.vue
+++ src/components/ExpansionPanels/FiltersSearchTicket.vue
@@ -17,10 +17,58 @@
             ></v-text-field>
           </v-col>
           <v-col cols="4">
-            <v-text-field
-              :label="inputField.estado.label"
-              :placeholder="inputField.estado.placeholder"
-            ></v-text-field>
+            <v-autocomplete
+              v-model="model"
+              :items="items"
+              :loading="isLoading"
+              :search-input.sync="search"
+              chips
+              clearable
+              hide-details
+              hide-selected
+              item-text="name"
+              item-value="symbol"
+              label="Search for a coin..."
+              solo
+            >
+              <template v-slot:no-data>
+                <v-list-item>
+                  <v-list-item-title>
+                    Search for your favorite
+                    <strong>Cryptocurrency</strong>
+                  </v-list-item-title>
+                </v-list-item>
+              </template>
+              <template v-slot:selection="{ attr, on, item, selected }">
+                <v-chip
+                  v-bind="attr"
+                  :input-value="selected"
+                  color="blue-grey"
+                  class="white--text"
+                  v-on="on"
+                >
+                  <v-icon left> mdi-bitcoin </v-icon>
+                  <span v-text="item.name"></span>
+                </v-chip>
+              </template>
+              <template v-slot:item="{ item }">
+                <v-list-item-avatar
+                  color="indigo"
+                  class="text-h5 font-weight-light white--text"
+                >
+                  {{ item.name.charAt(0) }}
+                </v-list-item-avatar>
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.name"></v-list-item-title>
+                  <v-list-item-subtitle
+                    v-text="item.symbol"
+                  ></v-list-item-subtitle>
+                </v-list-item-content>
+                <v-list-item-action>
+                  <v-icon>mdi-bitcoin</v-icon>
+                </v-list-item-action>
+              </template>
+            </v-autocomplete>
           </v-col>
         </v-row>
       </v-expansion-panel-content>
@@ -34,25 +82,53 @@ export default {
   data() {
     return {
       headerTitle: "Filtros de Tabla",
-      inputField:{
-        estado:{
-            label:'Estado',
-            placeholder: 'EJ: Abierto'
+      inputField: {
+        estado: {
+          label: "Estado",
+          placeholder: "EJ: Abierto",
         },
-        sector:{
-            label:'Sector',
-            placeholder: 'EJ: Gerencia de Provisioning' 
+        sector: {
+          label: "Sector",
+          placeholder: "EJ: Gerencia de Provisioning",
         },
-        numeroTicket:{
-            label:'Número de Ticket',
-            placeholder: 'EJ: 20220816DS' 
-        }
-      }
+        numeroTicket: {
+          label: "Número de Ticket",
+          placeholder: "EJ: 20220816DS",
+        },
+      },
+      isLoading: false,
+      items: [],
+      model: null,
+      search: null,
+      tab: null,
     };
   },
   methods: {
     //searchAPI (Vuex)
     //cleanFilters(Vuex)
+  },
+  watch: {
+    model(val) {
+      if (val != null) this.tab = 0;
+      else this.tab = null;
+    },
+    search() {
+      // Items have already been loaded
+      if (this.items.length > 0) return;
+
+      this.isLoading = true;
+
+      // Lazily load input items
+      fetch("https://api.coingecko.com/api/v3/coins/list")
+        .then((res) => res.clone().json())
+        .then((res) => {
+          this.items = res;
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+        .finally(() => (this.isLoading = false));
+    },
   },
 };
 </script>
